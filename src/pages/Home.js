6b19871cf91d9fced47725b06a9ed1dc4168f2e1
Laure <117588005+lorlorlorlorlor@users.noreply.github.com>
2023-05-15 12:16:25 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ class Header extends Component {
         <div id="homepage">
             <About portfolio = { this.props.portfolio } />
             <Portfolio portfolio = { this.props.portfolio } />
-            <Cool cool = { this.props.portfolio } />
+            <!--Cool cool = { this.props.portfolio } /-->
         </div>
       )
   }
